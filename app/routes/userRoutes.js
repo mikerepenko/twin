a67ff5cb409a52e920c2b6500a40e850959dc96b
Михаил Репенко <mikhail@MacBook-Pro-Mihail.local>
2023-09-27 09:47:43 +0300

    Fix

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -10,7 +10,7 @@ module.exports = function(app) {
 
   app.post('/GetProfile', async (req, res) => {
     try {
-      const user = await UserModel.findById(req.body.userId)
+      const user = await User.findById(req.body.userId)
 
       if (!user) {
         return res.status.apply(404).json({
@@ -36,7 +36,7 @@ module.exports = function(app) {
       if (req.body.options) {
         const { userId, name, age, description, gender }  = JSON.parse(req.body.options)
 
-        await UserModel.updateOne({ _id: userId }, {
+        await User.updateOne({ _id: userId }, {
           $set: {
             name,
             age,
@@ -57,7 +57,7 @@ module.exports = function(app) {
       // const likes = user._doc.likes
       // const dilikes = user._doc.dilikes
     
-      const users = await UserModel.find()
+      const users = await User.find()
       //console.log(users)
       res.json(users)
     } catch (err) { internalServerError() }
@@ -68,11 +68,11 @@ module.exports = function(app) {
       const { userId, humanId, like }  = req.body
 
       if (like) {
-        await UserModel.updateOne({ _id: userId }, {
+        await User.updateOne({ _id: userId }, {
           $set: {likes: [...new Set([...likes, humanId])]}
         })
       } else {
-        await UserModel.updateOne({ _id: userId }, {
+        await User.updateOne({ _id: userId }, {
           $set: {dilikes: [...new Set([...dilikes, humanId])]}
         })
       }
