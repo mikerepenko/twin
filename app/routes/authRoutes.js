4c95b8f2fea05206b9411468baf217845b57981f
Михаил Репенко <mikhail@MacBook-Pro-Mihail.local>
2023-09-27 09:52:12 +0300

    Fix

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -1,4 +1,4 @@
-const User = require('../models/user.js')
+const User = require('../models/User.js')
 const { internalServerError, generateCode } = require('../utils.js')
 
 module.exports = function(app) {
