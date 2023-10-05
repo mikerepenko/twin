51950b078e1cb3e85f7a2fce3e1d7e5f10c970aa
Михаил Репенко <mikhail@MacBook-Pro-Mihail.local>
2023-10-05 11:49:36 +0300

    Fix

--- Controllers/chatController.js
+++ Controllers/chatController.js
@@ -2,20 +2,24 @@ const chatModel       = require('../Models/chatModel.js')
 const { catchErrors } = require('../utils.js')
 
 exports.createChat = catchErrors(async (req, res) => {
-  const { firstId, secondId } = req.body
+  try {
+    const { firstId, secondId } = req.body
 
-  const chat = await chatModel({
-    members: { $all: [firstId, secondId] },
-  })
-
-  //if (chat) res.status(200).json(chat)
-
-  const newChat = new chatModel({
-    members: [firstId, secondId],
-  })
-
-  const response = newChat.save()
-  res.status(200).json(response)
+    const chat = await chatModel({
+      members: { $all: [firstId, secondId] },
+    })
+  
+    //if (chat) res.status(200).json(chat)
+  
+    const newChat = new chatModel({
+      members: [firstId, secondId],
+    })
+  
+    const response = newChat.save()
+    res.status(200).json(response)
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 exports.getChats = catchErrors(async (req, res) => {
